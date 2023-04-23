29c0a2c1e34f5163749eec6eb94f2adad0889f6d
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-04-24 00:33:38 +0300

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const makeGalleryImages = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src= "${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`;
+
+const markupGallery = galleryItems
+  .map((item) => makeGalleryImages(item))
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markupGallery);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const imageUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">`);
+  instance.show();
+});
