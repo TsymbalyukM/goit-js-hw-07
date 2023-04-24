f6324871c3d2249ec1be47c4e73162168096a407
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-04-24 22:59:34 +0300

    add second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const makeGalleryImages = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`;
+
+const markupGallery = galleryItems
+  .map((item) => makeGalleryImages(item))
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markupGallery);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const galleryLightbox = new SimpleLightbox(".gallery__link", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
